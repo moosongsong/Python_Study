273025a4015df352605980682acdf2f33982fd36
moosongsong <50647845+moosongsong@users.noreply.github.com>
2020-09-05 17:09:54 +0900

    study py

---
+++ 200905_12_print_ex01.py
@@ -0,0 +1,7 @@
+mydata = ('Apple', 'Red', 600)
+# 문장 작성: name, color, price에 mydata의 item을 unpack하여 binding 한다
+
+print(f'{name} : price is {price}, color is {color}')
+
+# 빈칸 채우기: mydata를 argument unpack하여 예시와 같은 형태로 출력한다
+print('     '.format(   ))
