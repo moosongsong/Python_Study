d5bd4e0fbd313478728265aa000a5a5efc22a098
moosongsong <50647845+moosongsong@users.noreply.github.com>
2020-09-06 17:15:29 +0900

    study python!

---
+++ 200906_15_isNumber.py
@@ -0,0 +1,7 @@
+
+for _ in range(5):
+    a = input('문자 입력 : ')
+    if '0' <= a <= '9' :
+        print(f'{a}는 숫자입니다.')
+    else :
+        print(f'{a}는 숫자가 아닙니다.')
---
+++ 200906_15_test_unicode.py
@@ -0,0 +1,12 @@
+# https://home.unicode.org/
+a = 0x1F60A
+b = 0x1F9E1
+print(chr(a), chr(b))
+
+# 한글
+c = 0xAD00
+print(chr(c))
+
+# https://www.unicode.org/emoji/charts/emoji-list.html
+d = 0x1F63A
+print(chr(d))
