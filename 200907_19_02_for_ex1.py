991d6f8d1f4ace531c0a1a94d11699f91b0f4327
moosongsong <50647845+moosongsong@users.noreply.github.com>
2020-09-07 18:27:27 +0900

    study 2020 09 07

---
+++ 200907_19_02_for_ex1.py
@@ -0,0 +1,8 @@
+# 정수 두 개(a, b)를 입력 받아
+# a 부터 b까지 더하는 프로그램을 작성하라
+# 더한 결과를 result에 저장하여 출력한다
+
+a = int(input('Input the first num : '))
+b = int(input('Input the second num : '))
+
+
---
+++ 200907_19_03_for_ex2.py
@@ -0,0 +1,17 @@
+# 한 개의 정수 n을 입력 받아
+# for문을 사용하여 n! (Factorial)을 구하여 출력한다
+
+# n! = 1 * 2 * 3 * ... * n
+# 1 * 2 = 2
+# 2 * 3 = 6
+# 6 * 4 = 24
+# 24 * 5 = 120 => 5!
+
+n = int(input('input a number : '))
+
+
+
+# TIP
+# a = a + x  : a += x
+# a = a - x  : a -= x
+# *, /, //, %, ** : augmented assignment 
