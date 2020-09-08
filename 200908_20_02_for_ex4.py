822f84bdf04de259a7af939bac6443ee73c9a041
moosongsong <50647845+moosongsong@users.noreply.github.com>
2020-09-08 18:22:51 +0900

    study 2020 09 08

---
+++ 200908_20_02_for_ex4.py
@@ -0,0 +1,13 @@
+# 2차원 tuple을 주어진 형식에 맞게 바꾸어 출력하라
+# str.format(), f-string 등을 사용해 출력 형식을
+# 작성할 수 있다
+
+mydata = ( ('Apple', 'Red', 600),
+           ('Kiwi', 'Brown', 300),
+           ('Banana', 'Yellow', 200) )
+
+# Apple : price is 600, color is Red
+# Kiwi : price is 300, color is Brown
+# Banana : price is 200, color is Yellow
+
+
