d3eee155d3c69a589c4037ab2bb9ffce2b6598ae
moosongsong <songe08@gmail.com>
2021-07-14 17:47:46 +0900

    [fixed] day 10 이름 변경

---
+++ 210707_day10_반복문_디버깅/debugging_test.py
@@ -0,0 +1,15 @@
+filename = __file__
+# import pdb; pdb.set_trace()
+
+print(f'path = {filename}')
+a = 10
+b = 5
+c = a + 5
+d = a + b
+sumValue = 0
+breakpoint()
+
+for x in range(1, 100):
+    e = x + 2
+    print(e)
+    sumValue += e
